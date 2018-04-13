70e872541d12ee7a72e00e24d1b3d90e9cfb42cc
pseute <patrick.seute@gmail.com>
2018-04-12 21:43:47 -0400

    add script to change navbar color when scrolling

---
+++ script.js
@@ -0,0 +1,34 @@
+var myNav = document.getElementById('mynav');
+window.onscroll = function () { 
+    "use strict";
+    if ((window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0) >= 200 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("navbar-dark");
+        myNav.classList.add("navbar-light");
+        //myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        //myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+        myNav.classList.remove("navbar-light");
+        myNav.classList.add("navbar-dark");
+    }
+};
+
+var toggleBtn = document.querySelector('.navbar-toggler');
+var isToggled = false;
+toggleBtn.addEventListener('click', function(){
+	if(isToggled && ((window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0) >= 200 )){
+        isToggled = false;
+	} else if(isToggled){
+		myNav.classList.remove("nav-colored");
+        myNav.classList.remove("navbar-light");
+        myNav.classList.add("navbar-dark");
+        isToggled = false;
+	} else{
+		myNav.classList.add("nav-colored");
+		myNav.classList.remove("navbar-dark");
+		myNav.classList.add("navbar-light");
+		isToggled = true;
+	}
+});
